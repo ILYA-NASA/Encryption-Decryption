b1f612b77932d29d50c0b7f9f95ca2f630172991
ILYA.NASA <il.nasa@outlook.com>
2023-02-25 20:11:29 +0300

    type conversion

--- Topics/Processing strings/The percentage of G and C characters/src/Main.kt
+++ Topics/Processing strings/The percentage of G and C characters/src/Main.kt
@@ -2,6 +2,7 @@ fun main() {
     val input = readln()
     val gGcC = listOf('g', 'G', 'c', 'C')
     var count = 0
+
     input.forEach { if (gGcC.contains(it)) count++ }
     println(count.toDouble() / input.length * 100)
 }
