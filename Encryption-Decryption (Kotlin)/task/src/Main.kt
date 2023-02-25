dc49a4486517894a762fa1dabac3bad5a8fbb5dd
ILYA.NASA <il.nasa@outlook.com>
2023-02-25 22:04:32 +0300

    encryption using the user's key

--- Encryption-Decryption (Kotlin)/task/src/Main.kt
+++ Encryption-Decryption (Kotlin)/task/src/Main.kt
@@ -1,12 +1,21 @@
 package encryptdecrypt
 
+import kotlin.math.abs
+
 fun main() {
-    getEncrypt("we found a treasure!")
+    getEncrypt(readln(), readln().toInt())
 }
 
-fun getEncrypt(secret: String) {
-    secret.forEach { print(if (it.isLetter()) 'z' - (it - 'a') else it) }
-    // abcdefghijklmnopqrstuvwxyz
-    // zyxwvutsrqponmlkjihgfedcba
+fun getEncrypt(secret: String, key: Int) {
+    val alphabet = "abcdefghijklmnopqrstuvwxyz".toList()
+    secret.forEach {
+        print(
+            if (it.isLetter()) {
+                if (alphabet.indexOf(it) + key > alphabet.size)
+                    alphabet[abs(alphabet.size - (alphabet.indexOf(it) + key))]
+                else alphabet[alphabet.indexOf(it) + key]
+            } else it
+        )
+    }
 }
 
