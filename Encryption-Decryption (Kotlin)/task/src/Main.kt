80c06f1bcb3532950219e04bcb0d9a839eba401f
ILYA.NASA <il.nasa@outlook.com>
2023-03-01 15:37:44 +0300

    now my app work with command line

--- Encryption-Decryption (Kotlin)/task/src/Main.kt
+++ Encryption-Decryption (Kotlin)/task/src/Main.kt
@@ -1,9 +1,14 @@
 package encryptdecrypt
 
-fun main() = when (readln()) {
-    "enc" -> getEncrypt(readln(), readln().toInt())
-    "dec" -> getDecrypt(readln(), readln().toInt())
-    else -> print("invalid input")
+fun main(args: Array<String>) {
+    val mode = args[args.indexOf("-mode") + 1]
+    val key = args[args.indexOf("-key") + 1]
+    val data = args[args.indexOf("-data") + 1]
+    when (mode) {
+        "enc" -> getEncrypt(data, key.toInt())
+        "dec" -> getDecrypt(data, key.toInt())
+        else -> getEncrypt(data, key.toInt())
+    }
 }
 
 fun getEncrypt(futureSecret: String, key: Int) = futureSecret.forEach { print(it + key) }
