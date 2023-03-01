884127d0e557d049ea6532cd62650170e35247d6
ILYA.NASA <il.nasa@outlook.com>
2023-03-01 12:02:24 +0300

    ways to create arrays

---
+++ Topics/Arrays/Initializing an array of characters/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val characters = charArrayOf('a', 'z', 'e', 'd')
+    println(characters.joinToString())
+}
+
+//fun main() {
+//    val characters = "azed".toCharArray()
+//    println(characters.joinToString())
+//}
---
+++ Topics/Arrays/Initializing an array of integers/src/Main.kt
@@ -0,0 +1,12 @@
+@Suppress("MagicNumber")
+fun main() {
+    val numbers = intArrayOf(12, 17, 8, 101, 33)
+
+    // do not touch the lines below 
+    println(numbers.joinToString())
+}
+
+//fun main() {
+//    val numbers = "12 17 8 101 33".split(" ").map { it.toInt() }.toIntArray()
+//    println(numbers.joinToString())
+//}
