c21ef5a66042d598eba0d82f0d73e0b72b89818f
ILYA.NASA <il.nasa@outlook.com>
2023-03-04 20:06:37 +0300

    study progress

--- Encryption-Decryption (Kotlin)/task/src/Main.kt
+++ Encryption-Decryption (Kotlin)/task/src/Main.kt
@@ -4,15 +4,13 @@ fun main(args: Array<String>) {
     val mode = args[args.indexOf("-mode") + 1]
     val key = args[args.indexOf("-key") + 1]
     val data = args[args.indexOf("-data") + 1]
-    controller(mode = mode, key = key, data = data)
-}
-fun controller(mode: String = "enc", key: String = "0", data: String = "") {
     when (mode) {
-        "enc" -> printEncrypt(data, key.toInt())
-        "dec" -> printDecrypt(data, key.toInt())
+        "enc" -> getEncrypt(data, key.toInt())
+        "dec" -> getDecrypt(data, key.toInt())
+        else -> getEncrypt(data, key.toInt())
     }
 }
 
-fun printEncrypt(futureSecret: String, key: Int) = futureSecret.forEach { print(it + key) }
+fun getEncrypt(futureSecret: String, key: Int) = futureSecret.forEach { print(it + key) }
 
-fun printDecrypt(secret: String, key: Int) = secret.forEach { print(it - key) }
+fun getDecrypt(secret: String, key: Int) = secret.forEach { print(it - key) }
