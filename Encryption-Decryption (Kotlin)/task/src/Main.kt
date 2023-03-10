acf3eb88b881f4f7e7d51cadcf474df441323e50
ILYA.NASA <il.nasa@outlook.com>
2023-03-10 16:26:51 +0300

    interim solution

--- Encryption-Decryption (Kotlin)/task/src/Main.kt
+++ Encryption-Decryption (Kotlin)/task/src/Main.kt
@@ -1,16 +1,42 @@
 package encryptdecrypt
 
+import java.io.File
+
 fun main(args: Array<String>) {
     val mode = args[args.indexOf("-mode") + 1]
     val key = args[args.indexOf("-key") + 1]
     val data = args[args.indexOf("-data") + 1]
+    val inFileName = args[args.indexOf("-in") + 1]
+    val outFileName = args[args.indexOf("-out") + 1]
+    controller(mode = mode, key = key, data = data, inFileName = inFileName, outFileName = outFileName)
+}
+
+fun controller(
+    mode: String = "enc", key: String = "0", data: String = "",
+    inFileName: String = "", outFileName: String = "",
+) {
     when (mode) {
-        "enc" -> getEncrypt(data, key.toInt())
-        "dec" -> getDecrypt(data, key.toInt())
-        else -> getEncrypt(data, key.toInt())
+        "enc" -> writeFile(inFileName, key.toInt(), outFileName)
+        "dec" -> writeFile(inFileName, key.toInt(), outFileName)
     }
 }
 
-fun getEncrypt(futureSecret: String, key: Int) = futureSecret.forEach { print(it + key) }
+fun writeFile(inFileName: String, key: Int, outFileName: String = "") {
+    val inFile = File(inFileName)
+    val inFileData = inFile.readText()
+    println(inFileData)
+    val outFile = File(outFileName)
+    outFile.writeText(getEncrypt(inFileData, key))
+    outFile.writeText(getDecrypt(inFileData, key))
+}
 
-fun getDecrypt(secret: String, key: Int) = secret.forEach { print(it - key) }
+fun getEncrypt(data: String, key: Int): String {
+    var result = ""
+    data.forEach { result += it + key }
+    return result
+}
+fun getDecrypt(data: String, key: Int): String {
+    var result = ""
+    data.forEach { result += it - key }
+    return result
+}
