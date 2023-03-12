e4bab92092db3cda55b1029f14882ff60cf31679
ILYA.NASA <il.nasa@outlook.com>
2023-03-12 10:37:55 +0300

    returned penultimate string element

---
+++ Topics/Working with strings/The number of tens/src/Main.kt
@@ -0,0 +1,10 @@
+/*
+fun main() {
+    val input = readln()
+    println(input[input.length - 2])
+}
+
+fun main() = println(readln().reversed()[1])
+*/
+
+fun main() = println(readln().dropLast(1).last())
